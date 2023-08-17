6071ec8509c1577decaf17f7572f0a41e095cc81
Nicolas Depelteau <n.depelteau@lightxinnovations.com>
2023-08-17 12:26:26 -0400

    update to 1.4.1

--- AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallComposite.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICalling/Sources/CallComposite.swift
@@ -88,14 +88,14 @@ public class CallComposite {
         exitManager?.dismiss()
     }
 
-    convenience init(withOptions options: CallCompositeOptions? = nil,
-                     callingSDKWrapperProtocol: CallingSDKWrapperProtocol? = nil) {
+    public convenience init(withOptions options: CallCompositeOptions? = nil,
+                            callingSDKWrapperProtocol: CallingSDKWrapperProtocol? = nil) {
         self.init(withOptions: options)
         self.customCallingSdkWrapper = callingSDKWrapperProtocol
     }
 
-    convenience init(withOptions options: CallCompositeOptions? = nil,
-                     customSDKFactory: CustomSDKFactory) {
+    public convenience init(withOptions options: CallCompositeOptions? = nil,
+                            customSDKFactory: CustomSDKFactory) {
         self.init(withOptions: options)
         self.customSDKFactory = customSDKFactory
     }
