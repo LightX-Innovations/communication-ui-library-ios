3c1c356e1105e2c8c0989d9a88c00bc31d06a339
Nicolas Depelteau <n.depelteau@lightxinnovations.com>
2025-05-29 18:51:37 -0400

    Make state property public for improved accessibility

--- AzureCommunicationUI/sdk/AzureCommunicationUICommon/Sources/AzureCommunicationUICommon/Store.swift
+++ AzureCommunicationUI/sdk/AzureCommunicationUICommon/Sources/AzureCommunicationUICommon/Store.swift
@@ -7,7 +7,7 @@ import Foundation
 
 public class Store<State, Action>: ObservableObject {
 
-    @Published var state: State
+    public @Published var state: State
 
     private var dispatchFunction: CommonActionDispatch<Action>!
     private let reducer: Reducer<State, Action>
